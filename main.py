1d4ee75433009f84bbf101a60d2fb348ee7b6530
ellian-abe <ellian_abe@outlook.com>
2023-11-27 22:55:43 -0300

    remove login page

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import streamlit as st
+import pandas as pd
+from tabs import state_tab, comparision_tab
 
-st.set_page_config(layout="centered", page_title="Login")
+
+@st.cache_data
+def get_data():
+    df = pd.read_csv('data/report.csv', sep=',')
+
+    return df
+
+def main():
+    state, comparision = st.tabs(['state', 'comparision'])
+
+    df = get_data()
+
+    state_tab.main(state, df)
+    comparision_tab.main(comparision, df)
+
+# Page setting
+st.set_page_config(layout="wide", page_title="States HDI")
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-form = st.form("credentials")
-
-form.header("Login")
-username = form.text_input("Username")
-password = form.text_input("Passowrd", type="password")
-form.form_submit_button("Login")
+main()
